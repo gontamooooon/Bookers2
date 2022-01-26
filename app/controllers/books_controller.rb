556b37408c0644c6f2bd3ce7c057f38fa5a862e6
EC2 Default User <ec2-user@ip-172-31-21-167.ec2.internal>
2022-01-26 10:25:32 +0000

    [start]4-25

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      # リダイレクトと同時にフラッシュメッセージの実装
+      flash[:notice] = 'You have created book successfully'
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'You have updated book successfully.'
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice] = 'You have destroyed book successfully.'
+    redirect_to books_path
+  end
+
+  private
+  # ストロングパラメータ
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
